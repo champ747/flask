b16802bafa1b0e3da599f04aa6f64cad61fb8add
champ747 <102521447+champ747@users.noreply.github.com>
2024-11-11 02:21:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 from caferecommend import recommend_cafes as recommend_cafes_standard  # caferecommend.py의 함수 임포트
-from chatbot import recommend_cafes  # chatbot.py의 챗봇 추천 함수
 import json
 
 app = Flask(__name__)
@@ -11,15 +10,6 @@ def recommend():
     recommendations = recommend_cafes_standard(user_preferences)
     return app.response_class(
         response=json.dumps(recommendations, ensure_ascii=False),
-        mimetype='application/json'
-    )
-
-@app.route('/api/chatbot', methods=['POST'])
-def chatbot():
-    user_input = request.json.get('input')
-    recommendations = recommend_cafes(user_input)
-    return app.response_class(
-        response=json.dumps({"recommendations": recommendations}, ensure_ascii=False),
         mimetype='application/json'
     )
 
